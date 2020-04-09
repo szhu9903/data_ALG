0597d5af0777f470a7358789765c98d3c5393cec
15994092160 <1781082307@qq.com>
2020-04-09 21:34:54 +0800

    汉诺塔-暂未理解

--- src/data/testnode/TestRecursive.java
+++ src/data/testnode/TestRecursive.java
@@ -1,3 +1,4 @@
+
 package data.testnode;
 
 public class TestRecursive {
@@ -5,8 +6,10 @@ public class TestRecursive {
 
     public static void main(String[] args) {
 //        print(4);
-        int i = fibonacci(5);
-        System.out.println(i);
+//        int i = fibonacci(5);
+//        System.out.println(i);
+        hannoi(3,'A','B','C');
+
 
     }
 
@@ -17,12 +20,31 @@ public class TestRecursive {
         }
     }
 
-    //鏂愭尝閭ｅ
+    //斐波那契
     public static int fibonacci(int n) {
         if (n == 1 || n==2) {
             return 1;
         }else{
             return fibonacci(n-1) + fibonacci(n-2);
+        }
+
+    }
+
+    /**
+     *
+     * @param n    共有几个盘子
+     * @param sta  开始柱子
+     * @param in   中间柱子
+     * @param end  目标柱子
+     */
+    //汉诺塔
+    public  static void hannoi(int n,char sta, char in, char end) {
+        if (n == 1) {
+            System.out.println("第1个盘子从"+sta+"移动到"+end);
+        }else {
+            hannoi(n-1,sta,end,in);
+            System.out.println("第"+ n +"个盘子从"+sta+"移动到"+end);
+            hannoi(n-1,in,sta,end);
         }
 
     }
