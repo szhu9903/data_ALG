54db4680230511bcd6204d6dc5872f22f7194eda
15994092160 <1781082307@qq.com>
2020-04-30 21:31:53 +0800

    delete

--- src/tree/ArrayTree/TestArrayTree.java
+++
@@ -1,9 +0,0 @@
-package tree.ArrayTree;
-public class TestArrayTree {
-    public static void main(String[] args) {
-        int[] data = new int[] {1,2,3,4,5,6,7};
-        ArrayBinaryTree tree = new ArrayBinaryTree(data);
-        tree.frontShow();
-    }
-
-}
